a2955b730e7e89e679134ca38c2d64227d4039bd
Pedro3DS <pedroca2787@gmail.com>
2023-10-13 14:17:45 -0400

    update

--- javascript/login.js
+++ javascript/login.js
@@ -1,11 +1,20 @@
 function login(){
-    var userbd = ["Pedro"]
+    var userbd = {
+        "email" : "Pedro",
+        "senha" : "123"
+    }
     var username = document.getElementById("user_input").value
+    const password = document.getElementById("password").value
     
-    if (userbd.includes(username)){
-        var url = "veterinarismain.html"
-        window.close()
-        window.open(url)
+    if (userbd["email"].includes(username)){
+        if (userbd["senha"].includes(password)){
+            var url = "veterinarismain.html"
+            window.open(url)
+            window.close()
+        }
+        else{
+            document.getElementById("card").style.display = "block"
+        }
     }
     else{
         document.getElementById("card").style.display = "block"
